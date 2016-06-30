b3198b8b9d96fb5299ff8df4844685cfda8add40
Brett Neese <brett@neese.rocks>
2016-06-29 21:51:32 -0500

    fix code

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ var save = function(data){
 
             //pushing to DynamoDB
             docClient.put(params, function(err, data) {
-                if (err) logger.error(JSON.stringify(err, null, 2), logger.error(JSON.stringify(data, null, 2) );
+                if (err) logger.error(JSON.stringify(err, null, 2)); logger.error(JSON.stringify(data, null, 2));
             });
           });
         });
